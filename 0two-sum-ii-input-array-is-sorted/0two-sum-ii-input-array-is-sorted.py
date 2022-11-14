c70dea27bf67b5be4bfc65560ad9f4673154523e
J <60500256+Heo-Jeong-Eun@users.noreply.github.com>
2022-11-14 15:09:10 +0900

    Time: 365 ms, Memory: 14.9 MB - LeetHub

---
+++ 0two-sum-ii-input-array-is-sorted/0two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,8 @@
+import bisect
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i,num in enumerate(numbers):
+            expected = target - num
+            ind = bisect.bisect_left(numbers,expected,i+1)
+            if ind < len(numbers) and numbers[ind] == expected:
+                return [i+1,ind+1]
